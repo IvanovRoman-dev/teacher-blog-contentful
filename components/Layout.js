264ef861182757c60c6814e7c98635d29f170cc9
IvanovRoman-dev <79038554+IvanovRoman-dev@users.noreply.github.com>
2022-10-16 15:49:30 +0300

    adding favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
 import Particles from "react-particles";
@@ -20,6 +21,33 @@ const Layout = ({ children }) => {
    return (
       <>
          <div className="layout">
+            <Head>
+               <link
+                  rel="apple-touch-icon"
+                  sizes="180x180"
+                  href="/apple-touch-icon.png"
+               />
+               <link
+                  rel="icon"
+                  type="image/png"
+                  sizes="32x32"
+                  href="/favicon-32x32.png"
+               />
+               <link
+                  rel="icon"
+                  type="image/png"
+                  sizes="16x16"
+                  href="/favicon-16x16.png"
+               />
+               <link rel="manifest" href="/site.webmanifest" />
+               <link
+                  rel="mask-icon"
+                  href="/safari-pinned-tab.svg"
+                  color="#5bbad5"
+               />
+               <meta name="msapplication-TileColor" content="#00aba9" />
+               <meta name="theme-color" content="#ffffff" />
+            </Head>
             <Header />
             <main className="main">{children}</main>
             <Footer />
